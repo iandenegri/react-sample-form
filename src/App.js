20a4a38346987a15d7fc298b2cd3fb293e273989
Ian Denegri <iandenegri@hotmail.com>
2018-11-08 14:08:01 -0500

    Uploading form. Just do 'npm start' to get the form up and running in your browser. In case it fails to open the browser the localhost port is 3000.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Form from './Form.js';
 
 class App extends Component {
+
+  state = {
+    fields: {}
+  };
+
+  onSubmit = (fields) => {
+    this.setState({fields});
+    console.log('App component got: ', fields);
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Form onSubmit={fields => this.onSubmit(fields)}/>
       </div>
     );
   }
